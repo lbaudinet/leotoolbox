85090644386e143ba395ebd2dec1aaae4b0c0425
Léo Baudinet <68195951+lbaudinet@users.noreply.github.com>
2020-08-31 14:15:09 +0200

    Update lib_test.py

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -5,15 +5,20 @@ import os
 import leotoolbox
 import pandas as pd
 # Import from our lib
-from leotoolbox.lib import clean_data
+from leotoolbox.lib import get_data, data_projection, data_reconstruction
 import pytest
 
 
-def test_clean_data():
-    datapath = os.path.dirname(os.path.abspath(leotoolbox.__file__)) + '/data'
-    df = pd.read_csv('{}/data.csv.gz'.format(datapath))
-    first_cols = ['id', 'civility', 'birthdate', 'city', 'postal_code', 'vote_1']
-    assert list(df.columns)[:6] == first_cols
-    assert df.shape == (999, 142)
-    out = clean_data(df)
-    assert out.shape == (985, 119)
+def test_get_data():
+    faces = get_data()
+    assert faces.images.shape == (1288, 50, 37)
+    assert faces.data.shape == (1288, 1850)
+
+def test_data_projection():
+    faces = get_data()
+    assert data_projection(faces)[0].shape == (1288, 150)
+
+def test_data_reconstruction():
+    faces = get_data()
+    a,b = data_projection(faces)
+    assert data_reconstruction(a,b).shape == (1288, 1850)
